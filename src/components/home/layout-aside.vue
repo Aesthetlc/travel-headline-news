42efb53fa7030873c7d35ad45b31f1f31e1fa00d
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-09-18 22:40:39 +0800

    首页左侧导航栏完成

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="layout-aside">
+    <div class="title">
+      <img src="../../assets/img/logo_admin.png" alt />
+    </div>
+    <el-menu
+      class="el-menu-vertical-demo"
+      background-color="#353b4e"
+      text-color="#adafb5"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="1">
+        <i class="el-icon-s-home"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-document"></i>
+          <span>内容管理</span>
+        </template>
+        <el-menu-item index="1-1">发布文章</el-menu-item>
+        <el-menu-item index="1-2">内容列表</el-menu-item>
+        <el-menu-item index="1-3">评论列表</el-menu-item>
+        <el-menu-item index="1-4">素材管理</el-menu-item>
+      </el-submenu>
+
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-s-custom"></i>
+          <span>粉丝管理</span>
+        </template>
+        <el-menu-item index="1-1">图文数据</el-menu-item>
+        <el-menu-item index="1-2">粉丝概况</el-menu-item>
+        <el-menu-item index="1-3">粉丝画像</el-menu-item>
+        <el-menu-item index="1-4">粉丝列表</el-menu-item>
+      </el-submenu>
+
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">账户信息</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.layout-aside {
+  background-color: #323745;
+  height: 100vh;
+  width: 200px;
+  .title {
+    text-align: center;
+    padding: 15px 0px;
+    background-color: #2e2f32;
+    img {
+      height: 30px;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>我是主页</div>
+  <el-container>
+    <el-aside>
+      <layout-aside></layout-aside>
+    </el-aside>
+    <el-container>
+      <el-header>头</el-header>
+      <el-main>
+        <!-- 二级路由容器 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
 <style>
+
 </style>
