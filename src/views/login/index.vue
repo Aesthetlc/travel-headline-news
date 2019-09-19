177f566de0de64d317afadd797f67b9bf97b934f
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-09-19 16:49:21 +0800

    将index背景图片改为视频背景

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,37 +1,43 @@
 <template>
   <div class="login-index">
-    <el-card class="box-card">
-      <div class="title">
-        <img src="../../assets/img/logo_index.png" alt />
-      </div>
-      <!-- 表单验证：
+    <div class="masking">
+      <el-card class="box-card">
+        <div class="title">
+          <img src="../../assets/img/logo_index.png" alt />
+        </div>
+        <!-- 表单验证：
         1.只需要通过 rules 属性传入约定的验证规则，
         2.将 Form-Item 的 prop 属性设置为需校验的字段名即可。
-      -->
-      <!--
+        -->
+        <!--
           :model ===>表单数据对象
           :rules===>验证规则
-      -->
-      <el-form class="content" :model="formData" :rules="loginrules" ref="loginForm">
-        <el-form-item prop="mobile">
-          <el-input v-model="formData.mobile" placeholder="请输入手机号"></el-input>
-        </el-form-item>
-        <el-form-item prop="code">
-          <el-input v-model="formData.code" style="width:70%" placeholder="请输入验证码"></el-input>
-          <el-button style="float:right;width:28%">发送验证码</el-button>
-        </el-form-item>
-        <el-form-item prop="agree">
-          <el-checkbox v-model="formData.agree">
-            我已阅读并同意
-            <a>用户协议</a>和
-            <a>隐私条款</a>
-          </el-checkbox>
-        </el-form-item>
-        <el-form-item>
-          <el-button @click="loginForm()" type="primary" style="width:100%">登录</el-button>
-        </el-form-item>
-      </el-form>
-    </el-card>
+        -->
+        <el-form class="content" :model="formData" :rules="loginrules" ref="loginForm">
+          <el-form-item prop="mobile" class="temp">
+            <el-input class="a" v-model="formData.mobile" placeholder="请输入手机号"></el-input>
+          </el-form-item>
+          <el-form-item prop="code">
+            <el-input v-model="formData.code" style="width:70%" placeholder="请输入验证码"></el-input>
+            <el-button style="float:right;width:28%">发送验证码</el-button>
+          </el-form-item>
+          <el-form-item prop="agree">
+            <el-checkbox v-model="formData.agree">
+              <span>我已阅读并同意</span>
+              <a>用户协议</a>
+              <span>和</span>
+              <a>隐私条款</a>
+            </el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button @click="loginForm()" type="primary" style="width:100%">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
+    <video autoplay loop muted v-on:canplay="canplay">
+      <source src="../../assets/video/beach.mp4" type="video/mp4" />浏览器不支持 video 标签，建议升级浏览器。
+    </video>
   </div>
 </template>
 
@@ -39,14 +45,15 @@
 export default {
   data () {
     let validator = function (rule, value, callback) {
-    //   if (value) {
-    //     callback()
-    //   } else {
-    //     callback(new Error('请勾选相关条款'))
-    //   }
+      //   if (value) {
+      //     callback()
+      //   } else {
+      //     callback(new Error('请勾选相关条款'))
+      //   }
       value ? callback() : callback(new Error('请勾选相关条款'))
     }
     return {
+      vedioCanPlay: false,
       formData: {
         mobile: '',
         code: '',
@@ -77,6 +84,9 @@ export default {
     }
   },
   methods: {
+    canplay () {
+      this.vedioCanPlay = true
+    },
     loginForm () {
       // validate是一个方法
       // 方法中传入的一个函数
@@ -91,12 +101,13 @@ export default {
             console.log(res)
             window.localStorage.setItem('user-token', res.data.data.token)
             this.$router.push('/home')
-          }).catch(() => {
-            this.$message({
-              message: '您的手机号或者验证码有误哦',
-              type: 'warning'
-            })
           })
+            .catch(() => {
+              this.$message({
+                message: '您的手机号或者验证码有误哦',
+                type: 'warning'
+              })
+            })
         }
       })
     }
@@ -104,29 +115,49 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style>
+.el-form-item__error {
+  font-size: 14px !important;
+  color: yellow !important;
+}
+</style>
+<style lang='less' scoped>
 .login-index {
-  background: url("../../assets/img/login_bg.jpg");
+  // background: url('../../assets/img/login_bg.jpg');
   /* 相当于高度100% */
-  height: 100vh;
   /* 自适应高度 */
-  background-size: cover;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .box-card {
-    width: 576px;
-    height: 358px;
-    .title {
-      text-align: center;
-      img {
-        height: 50px;
+  // background-size: cover;
+  position: relative;
+  z-index: 0;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  .masking {
+    position: absolute;
+    z-index: 1;
+    height: 100vh;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .box-card {
+      position: absolute;
+      background-color: rgba(255, 255, 255, 0.2);
+      width: 576px;
+      height: 358px;
+      z-index: 2;
+      .title {
+        text-align: center;
+        img {
+          height: 70px;
+        }
       }
-    }
-    .content {
-      margin: 20px 20px;
-      a {
-        color: rgb(50, 150, 250);
+      .content {
+        margin: 20px 20px;
+        a {
+          color: rgb(50, 150, 250);
+        }
       }
     }
   }
