dd5bc5077f8399e441b0e3203ed4d8840aa90102
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-09-20 14:21:08 +0800

    完成首页顶端 静态资源样式

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-row class="layout-header">
+    <el-col :span="22">
+      <i class="el-icon-document"></i>
+      <span class="title-header">江苏传智播客教育科技股份有限公司</span>
+    </el-col>
+    <el-col :span="2" class="userinfo">
+      <img src="../../assets/img/avatar.jpg" alt />
+      <span>一个小草莓</span>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.layout-header {
+  display: flex;
+  justify-content: space-between;
+  line-height: 50px;
+  .title-header,.el-icon-document {
+    font-size: 20px;
+  }
+  .el-icon-document{
+    margin-right: 4px;
+  }
+  .userinfo img{
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    vertical-align: middle;
+    margin-right: 4px;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,23 +1,28 @@
 <template>
   <el-container>
-    <el-aside>
+    <el-aside style="width:200px">
       <layout-aside></layout-aside>
     </el-aside>
     <el-container>
-      <el-header>头</el-header>
+      <el-header>
+        <layout-header style="height:50px"></layout-header>
+      </el-header>
       <el-main>
         <!-- 二级路由容器 -->
         <router-view></router-view>
       </el-main>
+
     </el-container>
   </el-container>
 </template>
 
 <script>
 import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 export default {
   components: {
-    'layout-aside': layoutAside
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
   }
 }
 </script>
