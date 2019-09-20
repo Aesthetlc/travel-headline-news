980f6de21968e3be29d910418ab2f5ef3523ede4
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-09-20 21:33:00 +0800

    导航守卫，只有携带token才能直接访问，否则踢回login页面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './permission'
 import Component from './components'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
---
+++ src/permission.js
@@ -0,0 +1,18 @@
+import router from './router.js'
+
+// 全局前置守卫
+router.beforeEach(function (to, from, next) {
+  // to:即将进入的路由
+  // from:正要离开的路由
+  // next：一定要调用该方法来 resolve 这个钩子。执行效果依赖 `next` 方法的调用参数
+  if (to.path.startsWith('/home')) {
+    // 如果是'/home'==>继续判断token是否存在
+    let token = window.localStorage.getItem('user-token')
+    token ? next() : next('/login')
+  } else {
+    // 不是'/home'就是'/login'
+    // 因为一级路由只有'/home','/login'所以放行
+    next()
+  }
+})
+export default router
