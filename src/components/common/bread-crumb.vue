be39ec97640da128f49fb94de9221d47c570e591
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-09-21 14:26:00 +0800

    新增全局面包屑组件

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- 面包屑分割符号 separator -->
+  <el-breadcrumb separator=">">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>
+      <!-- 插槽 具名插槽 -->
+      <sloat name="title"></sloat>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 // 声明一个全局的组件，来进行整理
 import layoutAside from '../components/home/layout-aside'
 import layoutHeader from '../components/home/layout-header'
+import breadCrumb from '../components/common/bread-crumb'
 
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('layout-Crumb', breadCrumb)
   }
 }
