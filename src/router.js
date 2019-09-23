6d21492d2248a228fb2b83db81c702e2d16e47b5
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-09-23 22:36:38 +0800

    新增内容列表路由

--- src/router.js
+++ src/router.js
@@ -31,6 +31,11 @@ export default new Router({
     {
       path: 'material',
       component: () => import('./views/material')
+    },
+    // 实现按需加载(内容列表)
+    {
+      path: 'articles',
+      component: () => import('./views/articles')
     }
     ]
   }
